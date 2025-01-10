7d16e15dfeb7df99b6838daeb09103ede3d24f5f
WayChungChen <847927640@qq.com>
2025-01-10 21:42:18 +0800

    适配carwith多行歌词

--- android/src/main/java/com/guichaguri/trackplayer/service/metadata/MetadataManager.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/metadata/MetadataManager.java
@@ -290,9 +290,8 @@ public class MetadataManager {
         metadata.putLong(METADATA_KEY_DURATION, duration);
     
         if (lyric != null) {
-          Log.i("MetadataManager", "歌手：" + artist);
-          metadata.putString("ucar.media.metadata.UCAR_TITLE", title);
-          metadata.putString("ucar.media.metadata.UCAR_ARTIST", artist);
+        //   metadata.putString("ucar.media.metadata.UCAR_TITLE", title);
+        //   metadata.putString("ucar.media.metadata.UCAR_ARTIST", artist);
           metadata.putString("ucar.media.metadata.LYRICS_LINE", lyric);
         }
     
